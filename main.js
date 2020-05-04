f4a3fcef01fed0a27d9703bbef75b73e12d4f8d9
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-04 10:10:03 -0400

    Bug fix

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ prompt.get(['project1', 'project2'], function (err, result) {
 prompt.stop();
 
 Scratch.UserSession.load(function(err, user) { //get the username/password from file or prompt for it
+  
   user.cloudSession(project1, function(err, cloud) { //start a cloud session
     cloud.on('set', function(name, value) { //when any variable is set,
       console.log("Project 1:", name, value); //log it
       cloud.set(name, "Project 1:"+value) //set it to itself +1
     });
   });
-});
 
   user.cloudSession(project2, function(err, cloud) { //start a cloud session (project 2)
     cloud.on('set', function(name, value) { //when any variable is set,
