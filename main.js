2ab18162c5a8840b64a25e3553090e522da6ed67
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-05 11:28:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ prompt.get(['project1', 'project2'], function (err, result) {
   
     user.cloudSession(project1, function(err, cloud) { //start a cloud session
       cloud.on('set', function(name, value) { //when any variable is set,
-        console.log("Project 1:", name, value); //log it
-        cloud.set(name, "Project 1:"+value) //set it to itself +1
+        console.log("Project 1:", name, " changed to ", value, "."); //log it
+        cloud.set(name, value+1) //set it to itself +1
       });
     });
 
     user.cloudSession(project2, function(err, cloud) { //start a cloud session (project 2)
       cloud.on('set', function(name, value) { //when any variable is set,
-        console.log("project 2:", name, "Project 2:",value); //log it
-        cloud.set(name, "Project 2:"+value+1) //set it to itself +1
+        console.log("Project 2:", name, " changed to ", value, "."); //log it
+        cloud.set(name, value+1) //set it to itself +1
       });
     });
   });
