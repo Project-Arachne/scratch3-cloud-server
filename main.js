501d4ae90e5ce0937b3b6009555a9a040996c83d
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-05 10:09:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,22 +10,23 @@ prompt.get(['project1', 'project2'], function (err, result) {
   if (err) {console.log("Error:", err);}
   var project1= result.project1;
   var project2= result.project2;
-  });
-prompt.stop();
-
-Scratch.UserSession.load(function(err, user) { //get the username/password from file or prompt for it
   
-  user.cloudSession(project1, function(err, cloud) { //start a cloud session
-    cloud.on('set', function(name, value) { //when any variable is set,
-      console.log("Project 1:", name, value); //log it
-      cloud.set(name, "Project 1:"+value) //set it to itself +1
+  Scratch.UserSession.load(function(err, user) { //get the username/password from file or prompt for it
+  
+    user.cloudSession(project1, function(err, cloud) { //start a cloud session
+      cloud.on('set', function(name, value) { //when any variable is set,
+        console.log("Project 1:", name, value); //log it
+        cloud.set(name, "Project 1:"+value) //set it to itself +1
+      });
     });
-  });
 
-  user.cloudSession(project2, function(err, cloud) { //start a cloud session (project 2)
-    cloud.on('set', function(name, value) { //when any variable is set,
-      console.log("project 2:", name, "Project 2:",value); //log it
-      cloud.set(name, "Project 2:"+value+1) //set it to itself +1
+    user.cloudSession(project2, function(err, cloud) { //start a cloud session (project 2)
+      cloud.on('set', function(name, value) { //when any variable is set,
+        console.log("project 2:", name, "Project 2:",value); //log it
+        cloud.set(name, "Project 2:"+value+1) //set it to itself +1
+      });
     });
   });
 });
+prompt.stop();
+
