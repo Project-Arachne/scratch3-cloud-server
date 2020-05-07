5a088c0bed91872c31e3781edbbc9de9d01d83d1
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-07 19:25:42 -0400

    Create err_test.js

---
+++ err_test.js
@@ -0,0 +1,7 @@
+var Scratch = require('scratch-api');
+//const prompt = require('prompt');
+Scratch.UserSession.load(function(err, user) {
+
+user.getProject(id, function(err,project){
+  console.log("Got data:", project);
+}
