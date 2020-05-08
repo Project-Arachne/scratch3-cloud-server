e1e222fe9c1c7be3b7001e69f1600c3516c9d3e9
John Locke <51202569+J0hnL0cke@users.noreply.github.com>
2020-05-08 10:09:16 -0400

    Update err_test.js

--- err_test.js
+++ err_test.js
@@ -3,6 +3,7 @@ var id=392549179;
 //const prompt = require('prompt');
 Scratch.UserSession.load(function(err, user) {
   user.getProject(id, function(err,project){
+    if (err) {console.log("Error:",err);};
     console.log("Got data:", project);
   });
 });
